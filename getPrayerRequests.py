a6b2fbba870991aca05d4cf703c5f93eaa064e35
Toni Oluwole <59587344+tonioluwole@users.noreply.github.com>
2024-09-17 17:39:35 -0400

    implemented refresh

--- getPrayerRequests.py
+++ getPrayerRequests.py
@@ -6,7 +6,10 @@ from time import sleep
 import tkinter
 from tkinter import font
 from tkinter import *
-from apscheduler.scheduler import Scheduler
+from tkinter import messagebox
+from tkinter.simpledialog import askstring
+from tkinter.messagebox import showinfo
+
 
 #Auth codes and constants
 clientid='5ef5d12e37f08560522e850519259a5c03430635b3ac60b2477d0476a0cb52cc'
@@ -20,121 +23,122 @@ username = os.getlogin()
 t= date.today()
 today=t.strftime('%m-%d-%Y')
 
-#API Call to get json data
-x = requests.get('https://api.planningcenteronline.com/people/v2/forms/818055/form_submissions?include=form_submission_values', 
-auth=(clientid,secret))
+def prayerrequests():
+    #API Call to get json data
+    x = requests.get('https://api.planningcenteronline.com/people/v2/forms/818055/form_submissions?include=form_submission_values', 
+    auth=(clientid,secret))
 
-# print("x= ",x.status_code)
-# 200 Success.
-# 301 Moved Permanently: site moved, redirect.
-# 302 Found: Temporary redirect.
-# 400 Bad Request: Client error.
-# 401 Unauthorized: Authentication needed.
-# 403 Forbidden: Access denied.
-# 404 Not Found: Resource missing.
-# 500 Internal Server Error: Server issue.
-# 503 Service Unavailable: Server overloaded or down.. 
+    # print("x= ",x.status_code)
+    # 200 Success.
+    # 301 Moved Permanently: site moved, redirect.
+    # 302 Found: Temporary redirect.
+    # 400 Bad Request: Client error.
+    # 401 Unauthorized: Authentication needed.
+    # 403 Forbidden: Access denied.
+    # 404 Not Found: Resource missing.
+    # 500 Internal Server Error: Server issue.
+    # 503 Service Unavailable: Server overloaded or down.. 
 
-#Convert request to JSON data called json_data
-json_data = x.json()
+    #Convert request to JSON data called json_data
+    json_data = x.json()
 
-#Converts data to python object
-json_object=json.loads(json.dumps(json_data))
+    #Converts data to python object
+    json_object=json.loads(json.dumps(json_data))
 
-#loop through and get the "display value" value from json data then write it to file. Should be the form submission
-filepath = "C:\\Users\\"+username+"\\Desktop\\"+today+"'s Prayer requests.txt" #Writes to desktop folder of PC running application
-with open(filepath, 'w+') as f: 
-    for i in json_object["included"]:
-        print ("--------\n",i["attributes"]['display_value'])
-        f.write(str("---------\n" + i["attributes"]['display_value']) + "\n\n" )
+    #loop through and get the "display value" value from json data then write it to file. Should be the form submission
+    filepath = "C:\\Users\\"+username+"\\Desktop\\"+today+"'s Prayer requests.txt" #Writes to desktop folder of PC running application
+    with open(filepath, 'w+') as f: 
+        for i in json_object["included"]:
+            print ("--------\n",i["attributes"]['display_value'])
+            f.write(str("---------\n" + i["attributes"]['display_value']) + "\n\n" )
 
-requestsfile=open(filepath)
-gottenrequests = requestsfile.read() #takes contents of textfile and creates string object
+    requestsfile=open(filepath)
 
-#alternative to GUI window creation
-#os.startfile(filepath)
+    gottenrequests = requestsfile.read()
+    #takes contents of textfile and creates string object
 
-"""
-------------------------- START OF GUI / Tkinter ---------------------------
-"""
-#Function to display all tkinter fonts
-def allfonts():
-    root = Tk()
-    root.title('Font Families')
-    fonts=list(font.families())
-    fonts.sort()
+    #alternative to GUI window creation
+    #os.startfile(filepath)
 
-    def populate(frame):
-        '''Put in the fonts'''
-        listnumber = 1
-        for i, item in enumerate(fonts):
-            label = "listlabel" + str(listnumber)
-            label = Label(frame,text=item,font=(item, 16))
-            label.grid(row=i)
-            label.bind("<Button-1>",lambda e,item=item:copy_to_clipboard(item))
-            listnumber += 1
+    """
+    ------------------------- START OF GUI / Tkinter ---------------------------
+    """
+    #Function to display all tkinter fonts
+    def allfonts():
+        root = Tk()
+        root.title('Font Families')
+        fonts=list(font.families())
+        fonts.sort()
 
-    def copy_to_clipboard(item):
-        root.clipboard_clear()
-        root.clipboard_append("font=('" + item.lstrip('@') + "', 12)")
+        def populate(frame):
+            '''Put in the fonts'''
+            listnumber = 1
+            for i, item in enumerate(fonts):
+                label = "listlabel" + str(listnumber)
+                label = Label(frame,text=item,font=(item, 16))
+                label.grid(row=i)
+                label.bind("<Button-1>",lambda e,item=item:copy_to_clipboard(item))
+                listnumber += 1
 
-    def onFrameConfigure(canvas):
-        '''Reset the scroll region to encompass the inner frame'''
-        canvas.configure(scrollregion=canvas.bbox("all"))
+        def copy_to_clipboard(item):
+            root.clipboard_clear()
+            root.clipboard_append("font=('" + item.lstrip('@') + "', 12)")
 
-    canvas = Canvas(root, borderwidth=0, background="#ffffff")
-    frame = Frame(canvas, background="#ffffff")
-    vsb = Scrollbar(root, orient="vertical", command=canvas.yview)
-    canvas.configure(yscrollcommand=vsb.set)
+        def onFrameConfigure(canvas):
+            '''Reset the scroll region to encompass the inner frame'''
+            canvas.configure(scrollregion=canvas.bbox("all"))
 
-    vsb.pack(side="right", fill="y")
-    canvas.pack(side="left", fill="both", expand=True)
-    canvas.create_window((4,4), window=frame, anchor="nw")
+        canvas = Canvas(root, borderwidth=0, background="#ffffff")
+        frame = Frame(canvas, background="#ffffff")
+        vsb = Scrollbar(root, orient="vertical", command=canvas.yview)
+        canvas.configure(yscrollcommand=vsb.set)
 
-    frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+        vsb.pack(side="right", fill="y")
+        canvas.pack(side="left", fill="both", expand=True)
+        canvas.create_window((4,4), window=frame, anchor="nw")
 
-    populate(frame)
+        frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
 
+        populate(frame)
+
+        root.mainloop()
+    #END of all fonts
+
+    #Constants for GUI
+    root = Tk()  # create a root widget
+
+    root.title("Prayer Requests")
+    root.configure(background=navy)
+    root.minsize(200, 200)  # width, height
+    root.maxsize(1200, 800)
+    root.geometry("600x600+660+240")  # width x height + x + y
+
+    Body_font = ("YU Gothic UI", 14,) 
+    Label_font = ("YU Gothic UI", 18, 'bold') 
+
+    scrollbar = Scrollbar(root)
+    scrollbar.pack( side = RIGHT, fill=Y)
+    ###################
+
+    Mainlabel=Label(font=Label_font,text="Prayer Requests",background=grey, foreground=white,height=2, borderwidth=2, relief="groove")
+    Mainlabel.pack(fill='both', expand=False)
+
+    text = Text(root, yscrollcommand = scrollbar.set)
+    text.insert(INSERT,gottenrequests)
+    text.configure(font=Body_font,state=DISABLED, background=navy, foreground='White', borderwidth=0)
+    text.pack(fill='both', expand=True,padx=(50,50))
+    scrollbar.config( command = text.yview)
+    
     root.mainloop()
-#END of all fonts
 
-class prayerrequests():
-    def __init__(self):
-        threading.Timer(5.0, prayerrequests).start()
-        #Constants for GUI
-        self.root = Tk()  # create a root widget
+    yes = messagebox.askyesno('','Refresh prayer requests?')
+    return yes
 
-        self.root.title("Prayer Requests")
-        self.root.configure(background=navy)
-        self.root.minsize(200, 200)  # width, height
-        self.root.maxsize(1200, 800)
-        self.root.geometry("600x600+660+240")  # width x height + x + y
+prayerrequests()
 
-        Body_font = ("YU Gothic UI", 14,) 
-        Label_font = ("YU Gothic UI", 18, 'bold') 
-
-        scrollbar = Scrollbar(self.root)
-        scrollbar.pack( side = RIGHT, fill=Y)
-        ###################
-
-        Mainlabel=Label(font=Label_font,text="Prayer Requests",background=grey, foreground=white,height=2, borderwidth=2, relief="groove")
-        Mainlabel.pack(fill='both', expand=False)
-
-        text = Text(self.root, yscrollcommand = scrollbar.set)
-        text.insert(INSERT,gottenrequests)
-        text.configure(font=Body_font,state=DISABLED, background=navy, foreground='White', borderwidth=0)
-        text.pack(fill='both', expand=True,padx=(50,50))
-        scrollbar.config( command = text.yview )
-
-app = prayerrequests()
-
-def refresh(self):
-    self.destroy()
-    self.__init__()
-
-while True:
-    app.root.mainloop() #runs the app 
-    sleep(2)
-
-            
+#Loop to refresh app and get new requests, plan is to make it a button within the app
+if prayerrequests() == True:
+    prayerrequests()
+else:
+    quit()
 
